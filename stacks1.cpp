ac93d8f130cf2232850f5ecad76f1e94e21bdf77
Erik Papernyuk <80971885+Erp4759@users.noreply.github.com>
2024-10-24 20:42:44 +0500

    Update stacks1.cpp

--- stacks1.cpp
+++ stacks1.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <stack>
 
+using namespace std;
+
+void insertAtBottom(stack<int>& stk, int value) {
+    if (stk.empty()) {
+        stk.push(value);
+    } else {
+        int topElement = stk.top();
+        stk.pop();
+        insertAtBottom(stk, value);
+        stk.push(topElement);
+    }
+}
+
+void reverseStack(stack<int>& stk) {
+    if (!stk.empty()) {
+        int topElement = stk.top();
+        stk.pop();
+        reverseStack(stk);
+        insertAtBottom(stk, topElement);
+    }
+}
+
+int main() {
+    stack<int> stk;
+    int n;
+
+    cout << "Enter number of elements";
+    cin >> n;
+
+    cout << "Enter stack elements:" << endl;
+    for (int i = 0; i < n; ++n) {
+        int element;
+        cin >> element;
+        stk.push(element);
+    }
+
+    reverseStack(stk);
+
+    cout << "Stack after: ";
+    while (!stk.empty()) {
+        cout << stk.top() << " ";
+        stk.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
