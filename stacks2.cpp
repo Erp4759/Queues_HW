6126a7000a2177bd1e21df922036d2709b76bdc2
Erik Papernyuk <80971885+Erp4759@users.noreply.github.com>
2024-10-24 20:46:46 +0500

    Update stacks2.cpp

--- stacks2.cpp
+++ stacks2.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << " Enter elements: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    vector<int> nge(n, -1); // Вектор для хранения следующего большего элемента
+    stack<int> indices;     // Стек для хранения индексов элементов
+
+    for (int i = 0; i < n; ++i) { // Пока стек не пуст и текущий элемент больше элемента на вершине стека
+        while (!indices.empty() && arr[i] > arr[indices.top()]) {
+            nge[indices.top()] = arr[i];
+            indices.pop();
+        }
+        indices.push(i);
+    }
+
+    cout << "Result:\n";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " --> " << (nge[i] == -1 ? -1 : nge[i]) << endl;
+    }
+
+    return 0;
+}
 
