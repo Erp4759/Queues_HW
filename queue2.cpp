e5f5681bea643ffc42dc868aadf375b858494f96
Erik Papernyuk <80971885+Erp4759@users.noreply.github.com>
2024-10-24 20:53:01 +0500

    Update queue2.cpp

--- queue2.cpp
+++ queue2.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    queue<int> q;
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+
+    int half = n / 2;
+    queue<int> firstHalf;
+
+    for (int i = 0; i < half; ++i) {
+        firstHalf.push(q.front());
+        q.pop();
+    }
+
+    while (!firstHalf.empty()) {
+        q.push(firstHalf.front());
+        firstHalf.pop();
+        q.push(q.front());
+        q.pop();
+    }
+
+    cout << "Result: ";
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
 
